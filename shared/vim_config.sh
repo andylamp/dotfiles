99a4606cc3046d3066540249b088b9ef029bcbfe
andylamp <andreas.grammenos@gmail.com>
2020-03-18 23:03:12 +0200

    fixed the git command.

--- shared/vim_config.sh
+++ shared/vim_config.sh
@@ -11,7 +11,7 @@ vim_config() {
   if [[ -d ${MY_HOME}.vim_runtime ]]; then
     cli_warning "vim_runtime dir already exists, probably already configured - trying to update it!"
     # updating from master
-    git -C ${MY_HOME}.vim_runtime git pull
+    git -C ${MY_HOME}.vim_runtime pull
   else
     cli_info "Installing awesome_vimrc to ${MY_HOME}."
     # clone the repository
