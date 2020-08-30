816ff61864041d6013d575565275e2f0409c7703
andylamp <andreas.grammenos@gmail.com>
2020-08-30 05:32:26 +0300

    altered bootstrap a bit.

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,7 @@ sourced=0
 if [[ -n "${ZSH_EVAL_CONTEXT}" ]]; then
   case ${ZSH_EVAL_CONTEXT} in *:file) sourced=1;; esac
 elif [[ -n "${KSH_VERSION}" ]]; then
-  [[ "$(cd $(dirname -- $0) && pwd -P)/$(basename -- $0)" != "$(cd $(dirname -- ${.sh.file}) && pwd -P)/$(basename -- ${.sh.file})" ]] && sourced=1
+  [[ "$(cd "$(dirname -- "$0")" && pwd -P)/$(basename -- $0)" != "$(cd "$(dirname -- ${.sh.file})" && pwd -P)/$(basename -- "${.sh.file}")" ]] && sourced=1
 elif [[ -n "${BASH_VERSION}" ]]; then
   (return 0 2>/dev/null) && sourced=1
 else # All other shells: examine $0 for known shell binary filenames
