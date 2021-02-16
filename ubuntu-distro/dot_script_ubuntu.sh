d12438665976c43cbaea0996c7c89d3bf2b4ee75
andylamp <andreas.grammenos@gmail.com>
2021-02-16 02:11:14 +0200

    reworked package installation.

--- ubuntu-distro/dot_script_ubuntu.sh
+++ ubuntu-distro/dot_script_ubuntu.sh
@@ -43,49 +43,76 @@ if ! wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg |
 	exit 1
 fi
 
-# Install (my) packages
-
-# install (my) essential packages while checking if packages installed correctly.
-if ! sudo apt update && sudo apt install -y valgrind \
-	graphviz \
-	vim \
-	curl \
-	git \
-	apt-transport-https \
-	sublime-text \
-	openjdk-11-jdk openjdk-11-doc \
-	build-essential \
-	python3 python3-dev \
-	checkinstall \
-	cpu-checker \
-	fonts-firacode \
-	tmux \
-	maven \
-	kitty \
-	openssh-server \
-	openssh-client \
-	glances \
-	htop \
-	python3-pip \
-	nvme-cli \
-	smartmontools \
-	jq; then
-	cli_error "Error, non-zero code encountered while installing essential packages - cannot continue."
+# Install (my) packages, but first try to refresh deb repository
+if ! sudo apt update; then
+	cli_error "Error, non-zero code encountered while updating apt local repository - cannot continue."
 	exit 1
 fi
 
+# helper function for installing packages
+function pkg_install() {
+	if ! sudo apt install -yy "${1}"; then
+		cli_error "Error, non-zero code encountered while installing package: ${package} - cannot continue."
+		return 1
+	fi
+}
+
+# (my) essential package list
+BASE_PACKAGE_LIST=(
+	"valgrind"
+	"graphviz"
+	"vim"
+	"curl"
+	"git"
+	"apt-transport-https"
+	"sublime-text"
+	"openjdk-11-jdk"
+	"openjdk-11-doc"
+	"build-essential"
+	"python3"
+	"python3-dev"
+	"checkinstall"
+	"cpu-checker"
+	"fonts-firacode"
+	"tmux"
+	"maven"
+	"kitty"
+	"openssh-server"
+	"openssh-client"
+	"glances"
+	"htop"
+	"python3-pip"
+	"nvme-cli"
+	"smartmontools"
+	"jq"
+)
+
+# my extended package list
+EXT_PACKAGE_LIST=(
+	"lm-sensors"
+	"sysstat"
+	"doxygen"
+	"python3-sphinx"
+	"qbittorrent"
+	"python3-bottle"
+)
+
+# install (my) essential packages while checking if packages installed correctly.
+
+cli_info "Installing my essential package list!"
+for package in "${BASE_PACKAGE_LIST[@]}"; do
+	if ! pkg_install "${package}"; then
+		cli_error "Error, non-zero code encountered while installing essential packages - cannot continue."
+		exit 1
+	fi
+done
+
 # install optional packages
 if [[ ${CFG_MINIMAL} = false ]]; then
-
-	# try to install packages.
-	if ! sudo apt install -y lm-sensors \
-		sysstat \
-		doxygen \
-		python3-sphinx \
-		qbittorrent \
-		python3-bottle; then
-		cli_error "Error, non-zero code encountered while installing extra packages - cannot continue."
-	fi
+	cli_info "Installing extended package list!"
+	for package in "${EXT_PACKAGE_LIST[@]}"; do
+		pkg_install "${package}"
+	done
 fi
 
 # copy ufw rules
